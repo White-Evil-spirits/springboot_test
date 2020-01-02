9f29a8c51253a2d2cf1338e9a648255fefeb86bf
zhangyuanshuo <zhangyuanshuo@qq.com>
2020-01-02 16:47:32 +0800

    菜单管理

--- src/main/java/com/zys/sys/vo/PermissionVo.java
+++ src/main/java/com/zys/sys/vo/PermissionVo.java
@@ -3,11 +3,25 @@ package com.zys.sys.vo;
 import com.zys.sys.domain.Permission;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
 
 @Data
 @EqualsAndHashCode(callSuper = false)
 public class PermissionVo extends Permission {
 
     private static final long serialVersionUID = 1L;
+
+    private Integer page = 1;
+    private Integer limit = 10;
+
+    //为了批量删除接收多个id
+    private Integer[] ids;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date strartTime;
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date endTime;
 
 }
